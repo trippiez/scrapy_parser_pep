f19b4ebc95cbf481de006bbd97cffadece6ec77a
Tom Bulmer <tombulmer@Toms-MacBook-Air.local>
2024-02-14 07:11:02 +0200

    add parsing number/name/status

--- pep_parse/spiders/pep.py
+++ pep_parse/spiders/pep.py
@@ -1,4 +1,6 @@
 import scrapy
+import re
+from pep_parse.items import PepParseItem
 
 
 class PepSpider(scrapy.Spider):
@@ -7,4 +9,30 @@ class PepSpider(scrapy.Spider):
     start_urls = ['http://peps.python.org/']
 
     def parse(self, response):
-        pass
+        dl_tag = response.xpath('//dl[contains(@class, "field-list")]')
+        for status in dl_tag:
+            text_status_tag = status.xpath('./dt[starts-with(., "Status")]')
+            pep_status = text_status_tag.xpath(
+                './following-sibling::dd//abbr/text()'
+            ).get()
+            pattern = r'^PEP (?P<number>\d+)\s–\s(?P<name>.*)$'
+            title = response.xpath('//h1[@class="page-title"]/text()').get()
+            re_match = re.search(pattern, title)
+            if re_match is not None:
+                number, name = re_match.groups()
+            else:
+                number, name = '', title
+            data = {
+                'number': number,
+                'name': name,
+                'status': pep_status
+            }
+            yield PepParseItem(data)
+
+        pep_links = response.xpath(
+            '//section[@id="numerical-index"]//tr/descendant::a/@href'
+        ).getall()
+
+        for pep_link in pep_links[:100]:
+            if pep_link is not None:
+                yield response.follow(pep_link, callback=self.parse)
